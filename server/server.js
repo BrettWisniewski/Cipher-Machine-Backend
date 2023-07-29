ab29534911123e7c7914b56685ce12c5b41e7632
Brett Wisniewski <bpw10va@gmail.com>
2023-07-29 10:19:08 -0600

    added cipher features

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.json()); // for parsing application/json
 
 
-// all of this is new database stuff
+// // all of this is new database stuff
 const { Pool } = require('pg');
 
 
@@ -16,51 +16,70 @@ const pool = new Pool({
   port: 5432, // Default PostgreSQL port is 5432
 });
 
+// Create a new table
+const createTableQuery = `
+  CREATE TABLE IF NOT EXISTS cipher_table (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    rules TEXT,
+    hints TEXT[],
+    answer JSONB
+  )
+`;
+
+pool.query(createTableQuery, (err, result) => {
+  if (err) {
+    console.error('Unable to create table:', err);
+  } else {
+    console.log('Table created successfully');
+  }
+});
+
 // Add mock data to a table
-const mockData = {
-    name: 'John Doe',
-    cipher: "RPTHPG",
-    rules: 'Mock data',
-  };
+// const mockData = {
+//     name: 'John Doe',
+//     cipher: "RPTHPG",
+//     rules: 'Mock data',
+//   };
   
 
-// Create a table
-pool.query(
-    `CREATE TABLE IF NOT EXISTS your_table (
-      id SERIAL PRIMARY KEY,
-      name VARCHAR(255),
-      cipher VARCHAR(255),
-      cipher_rules VARCHAR(255)
-    )`,
-    (error, results) => {
-      if (error) {
-        console.error('Error creating table:', error);
-      } else {
-        console.log('Table created or already exists!');
-      }
-    }
-  );
+// // Create a table
+// pool.query(
+//     `CREATE TABLE IF NOT EXISTS your_table (
+//       id SERIAL PRIMARY KEY,
+//       name VARCHAR(255),
+//       cipher VARCHAR(255),
+//       cipher_rules VARCHAR(255)
+//     )`,
+//     (error, results) => {
+//       if (error) {
+//         console.error('Error creating table:', error);
+//       } else {
+//         console.log('Table created or already exists!');
+//       }
+//     }
+//   );
   
-  // Check if the table exists
-  pool.query(
-    `SELECT EXISTS (
-      SELECT 1
-      FROM information_schema.tables
-      WHERE table_name = 'your_table'
-    )`,
-    (error, results) => {
-      if (error) {
-        console.error('Error checking table existence:', error);
-      } else {
-        const tableExists = results.rows[0].exists;
-        if (tableExists) {
-          console.log('Table exists!');
-        } else {
-          console.log('Table does not exist!');
-        }
-      }
-    }
-  );
+//   // Check if the table exists
+//   pool.query(
+//     `SELECT EXISTS (
+//       SELECT 1
+//       FROM information_schema.tables
+//       WHERE table_name = 'your_table'
+//     )`,
+//     (error, results) => {
+//       if (error) {
+//         console.error('Error checking table existence:', error);
+//       } else {
+//         const tableExists = results.rows[0].exists;
+//         if (tableExists) {
+//           console.log('Table exists!');
+//         } else {
+//           console.log('Table does not exist!');
+//         }
+//       }
+//     }
+//   );
 //   pool.query(
 //     'INSERT INTO your_table (name, age, email) VALUES ($1, $2, $3)',
 //     [mockData.name, mockData.age, mockData.email],
@@ -74,6 +93,121 @@ pool.query(
 //   );
 
 // end of database stuff
+
+
+// this is my view application
+
+
+
+// The commented code retrieves whole database
+// app.get('/data/allResults', (req, res) => {
+//   const getAllResultsQuery = `
+//     SELECT * FROM cipher_table
+//   `;
+
+//   pool.query(getAllResultsQuery, (err, result) => {
+//     if (err) {
+//       console.error('Error fetching all results:', err);
+//       res.sendStatus(500);
+//     } else {
+//       const results = result.rows;
+//       res.json(results);
+//     }
+//   });
+// });
+
+app.get('/data/allResults', (req, res) => {
+  const { name } = req.query;
+
+  const getAllResultsQuery = `
+    SELECT * FROM cipher_table
+    WHERE name = $1
+  `;
+
+  pool.query(getAllResultsQuery, [name], (err, result) => {
+    if (err) {
+      console.error('Error fetching all results:', err);
+      res.sendStatus(500);
+    } else {
+      const results = result.rows;
+      res.json(results);
+    }
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+app.get('/data/results', (req, res) => {
+  const { letter } = req.query;
+
+  const searchQuery = `
+    SELECT * FROM cipher_table
+    WHERE name LIKE '${letter}%'
+    LIMIT 3
+  `;
+
+  pool.query(searchQuery, (err, result) => {
+    if (err) {
+      console.error('Error fetching results:', err);
+      res.sendStatus(500);
+    } else {
+      
+      const results = result.rows;
+      res.json(results);
+    }
+  });
+});
+
+
+
+
+
+
+
+
+
+
+// end of my view application
+
+app.post('/data/reset', (req, res) => {
+  // const { insideData, nameSentence, theCipherRule, responses } = req.body;
+  console.log(req.body);
+  const { insideData, nameSetence, theCipherRule, responses } = req.body;
+  console.log(insideData, nameSetence, theCipherRule, responses);
+
+  // ... existing code
+
+  const values = [nameSetence || '', theCipherRule, responses, insideData];
+
+  const insertDataQuery = `
+    INSERT INTO cipher_table (name, rules, hints, answer)
+    VALUES ($1, $2, $3, $4)
+  `;
+
+  // const values = [nameSentence || '', theCipherRule, responses, insideData];
+
+  pool.query(insertDataQuery, values, (err, result) => {
+    if (err) {
+      console.error('Error inserting data:', err);
+      res.sendStatus(500);
+    } else {
+      console.log('Data inserted successfully');
+      res.sendStatus(200);
+    }
+  });
+});
+
+
+
 
 
 +
@@ -142,8 +276,148 @@ app.post('/api/submit', (req, res) => {
 
 
 })
+app.post('/api/submitcaesar', (req, res) => {
+  const { sentence } = req.body; // Access the 'sentence' property directly
+  let result = "";
+
+  for (let i = 0; i < sentence.length; i++) {
+    let charCode = sentence.charCodeAt(i);
+
+    if (charCode >= 65 && charCode <= 90) {
+      // Uppercase letters (A-Z)
+      charCode = ((charCode - 65 + 3) % 26) + 65;
+    } else if (charCode >= 97 && charCode <= 122) {
+      // Lowercase letters (a-z)
+      charCode = ((charCode - 97 + 3) % 26) + 97;
+    }
+
+    result += String.fromCharCode(charCode);
+  }
+
+  res.json({ sentence: result });
+
+  
+  })
+
+  app.post('/api/submitconcealment', (req, res) => {
+    try{
+    const { sentence } = req.body; // Access the 'sentence' property directly
+    // let cipheredString = "";
+    // loop through sentence
+    // for each letter, add three random words to the string
+    // for (let i = 0; i < sentence.length; i++) {
+    //   let charCode = sentence.charCodeAt(i);
+      
+    // }
+    
+    // const words = sentence.split(' '); // Split the sentence into an array of words
+    // const result = words.map((word) => {
+    //   const concealedWord = word.split('').map((char, index) => {
+    //     if (index > 0 && (index + 1) % 4 === 0) {
+    //       return '_'; // Conceal the character at the third position after each comma
+    //     }
+    //     return char;
+    //   });
+    //   return concealedWord.join('');
+    // });
+  
+    // const concealedSentence = result.join(', '); 
+
+    const letters = ['a', 'e', 'i', 'o', 'u', 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'y', 'z'];
+
+function randomWordGenerator(word) {
+  let counter = 0;
+  let returnedString = "";
+
+  while (counter <= 3) { 
+  if(counter === 2){returnedString += word}
+else{
+    const randomIndex = Math.floor(Math.random() * letters.length);
+    returnedString += letters[randomIndex];
+}
+    counter++; // Increment the counter
+  }
+  returnedString += ','
+
+  return returnedString;
+}
+
+function returnedThing(something) {
+  let blankstring = "";
+
+  for (let i = 0; i < something.length; i++) {
+    blankstring += randomWordGenerator(something[i]);
+    blankstring += " "
+  }
+
+  return blankstring; // Return blankstring, not result
+}
+
+    const indexedString = returnedThing(sentence);
+    res.json({ sentence: indexedString });
+}
+
+    catch (error) {
+      console.error('Error occurred:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  
+    
+    })
+
+  // app.post('/api/submittransposition', (req, res) => {
+  //   const { sentence } = req.body; // Access the 'sentence' property directly
+  //   let result = "";
+
+  // })
 
 // app.get("/caesar", a_middleware_function);
+
+app.post('/api/submittransposition', (req, res) => {
+try{
+  const { sentence } = req.body;
+  function routeCipher(theWord){
+    // remove whitespace in theWord 
+    let message = theWord.replace(/\s/g, '');
+
+    firstString = ""
+    secondString = ""
+    thirdString = ""
+    
+    counter = 0
+    
+    for (let i = 0; i < message.length; i++) {
+        if(counter === 0){
+  firstString += message[i];
+  counter += 1;
+        }
+        else if(counter === 1){
+            secondString += message[i];
+            counter += 1;
+        }
+        else{
+            thirdString += message[i]
+            counter = 0;
+        }
+        
+        
+}
+someString = firstString + "\n" + secondString +"\n" + thirdString
+return someString;
+    
+    
+}
+const result = routeCipher(sentence);
+res.json({ sentence: result });
+}
+
+catch (error) {
+  console.error('Error occurred:', error);
+  res.status(500).json({ error: 'Internal Server Error' });
+}
+
+
+})
 
 app.listen(5000, () => console.log('Server started on port 5000'));
 
